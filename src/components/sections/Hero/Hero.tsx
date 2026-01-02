b566972fab1ea0dac39bb7aa6d81d60025058a23
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 22:32:11 +0200

    Add WithMe component to Hero for improved user engagement and layout enhancement.

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,12 +1,14 @@
 import CreateYour from "./CreateYour"
 import HeroBackground from "./HeroBackground"
 import Feature from "./Feature"
+import WithMe from "./WithMy"
 
 function Hero() {
   return (
     <div className="relative h-screen overflow-hidden pt-44 px-4">
       <CreateYour />
       <Feature />
+      <WithMe />
       <HeroBackground />
     </div>
   )
---
+++ src/components/sections/Hero/WithMy.tsx
@@ -0,0 +1,27 @@
+
+import { useEffect, useState } from "react";
+import { Motion } from "@/motion/Motion";
+import WithMeSVG from "@/assets/banner/me.svg";
+
+function WithMe() {
+  const [animateImage, setAnimateImage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimateImage(true);
+    }, 600); // Delay for the image animation
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Motion animateOnMount={true} variant="fadeDown">
+      <h1 className="text-[40px] font-bold uppercase flex items-center">
+        <span className="">With</span>
+        <img src={WithMeSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[53px] object-cover rounded-lg  transition-all duration-500 ${animateImage ? 'w-[153px] opacity-100' : 'w-0 opacity-0'}`} />
+        <span >Me</span>
+      </h1>
+    </Motion> 
+  )
+}
+
+export default WithMe
