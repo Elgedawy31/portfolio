9d0cf9b37831b66b5cd67fea6f6532edc839ca3d
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 22:52:51 +0200

    Enhance WithMe component by introducing a content visibility toggle with a delay. Implement a new state to manage content display, allowing for a smoother transition between the initial image and the subsequent display, improving overall user experience and animation dynamics.

--- src/components/sections/Hero/WithMy.tsx
+++ src/components/sections/Hero/WithMy.tsx
@@ -5,22 +5,38 @@ import WithMeSVG from "@/assets/banner/me.svg";
 
 function WithMe() {
   const [animateImage, setAnimateImage] = useState(false);
+  const [showContent, setShowContent] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const initialImageTimer = setTimeout(() => {
       setAnimateImage(true);
-    }, 600); // Delay for the image animation
-    return () => clearTimeout(timer);
+    }, 600); // Delay for the initial image animation
+
+    const hideContentTimer = setTimeout(() => {
+      setShowContent(false);
+    }, 1200); // Delay to hide content and show big image after 1200ms
+
+    return () => {
+      clearTimeout(initialImageTimer);
+      clearTimeout(hideContentTimer);
+    };
   }, []);
 
   return (
-    <Motion animateOnMount={true} variant="fadeUp">
-      <h1 className="text-[40px] font-bold uppercase flex items-center">
-        <span className="">With</span>
-        <img src={WithMeSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[53px] object-cover rounded-lg  transition-all duration-500 ${animateImage ? 'w-[153px] opacity-100' : 'w-0 opacity-0'}`} />
-        <span >Me</span>
-      </h1>
-    </Motion> 
+  <>
+    <Motion animateOnMount={showContent} variant="fadeUp">
+        <h1 className="text-[40px] font-bold uppercase flex items-center relative">
+          <span className="">With</span>
+          <img src={WithMeSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[53px] object-cover rounded-lg transition-all duration-500 ${animateImage ? 'w-[153px] opacity-100' : 'w-0 opacity-0'}`} />
+          <span >Me</span>
+        </h1>
+    </Motion>
+    {!showContent && <h1 className="text-[40px] font-bold uppercase flex items-center relative">
+          <span className="opacity-0">With</span>
+          <img src={WithMeSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[53px] object-cover rounded-lg transition-all duration-500 ${animateImage ? 'w-[153px] opacity-100' : 'w-0 opacity-0'}`} />
+          <span className="opacity-0">Me</span>
+        </h1>}
+     </>
   )
 }
 
