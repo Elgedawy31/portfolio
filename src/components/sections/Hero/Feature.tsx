724392c187ba22eaa56740f6c975e906ad919e98
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 22:34:41 +0200

    Update Motion variants in Hero components to enhance animation effects: change Feature to slideLeft and WithMe to fadeUp for improved visual dynamics.

--- src/components/sections/Hero/Feature.tsx
+++ src/components/sections/Hero/Feature.tsx
@@ -14,7 +14,7 @@ function Feature() {
   }, []);
 
   return (
-    <Motion animateOnMount={true} variant="fadeDown">
+    <Motion animateOnMount={true} variant="slideLeft">
       <h1 className="text-[40px] font-bold uppercase flex items-center justify-end text-right">
         <span className={`${animateImage ? 'translate-0':'translate-x-8'} transition-transform relative`}>Feat</span>
         <img src={FeatureSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[29px] transition-all duration-500 ${animateImage ? 'w-[140px] opacity-100' : 'w-0 opacity-0'}`} />
--- src/components/sections/Hero/WithMy.tsx
+++ src/components/sections/Hero/WithMy.tsx
@@ -14,7 +14,7 @@ function WithMe() {
   }, []);
 
   return (
-    <Motion animateOnMount={true} variant="fadeDown">
+    <Motion animateOnMount={true} variant="fadeUp">
       <h1 className="text-[40px] font-bold uppercase flex items-center">
         <span className="">With</span>
         <img src={WithMeSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[53px] object-cover rounded-lg  transition-all duration-500 ${animateImage ? 'w-[153px] opacity-100' : 'w-0 opacity-0'}`} />
