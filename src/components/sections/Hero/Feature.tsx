f94c6dbdf14481bd1884b3d5d0367a0f28255de9
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 22:44:54 +0200

    Remove conditional rendering for the title in the Feature component to simplify the structure and enhance animation consistency. The title is now always displayed, improving visual clarity.

--- src/components/sections/Hero/Feature.tsx
+++ src/components/sections/Hero/Feature.tsx
@@ -24,13 +24,11 @@ function Feature() {
 
   return (
     <Motion animateOnMount={showContent} variant="slideLeft">
-      {showContent && (
         <h1 className="text-[40px] font-bold uppercase flex items-center justify-end text-right">
           <span className={`${animateImage ? 'translate-0':'translate-x-8'} transition-transform relative`}>Feat</span>
           <img src={FeatureSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[29px] transition-all duration-500 ${animateImage ? 'w-[140px] opacity-100' : 'w-0 opacity-0'}`} />
           <span >Ure</span>
         </h1>
-      )}
     </Motion> 
   )
 }
