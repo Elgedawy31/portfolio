345fe02f0cad35dd0fc838712c33d1312c648ab2
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 22:37:23 +0200

    Enhance CreateYour component by adding a content visibility toggle with a delay. Introduce a new state to manage content display and adjust Motion component to animate based on this state for improved user experience.

--- src/components/sections/Hero/CreateYour.tsx
+++ src/components/sections/Hero/CreateYour.tsx
@@ -5,21 +5,30 @@ import CreateYourSVG from "@/assets/banner/create-your.svg";
 
 function CreateYour() {
   const [animateImage, setAnimateImage] = useState(false);
+  const [showContent, setShowContent] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const imageTimer = setTimeout(() => {
       setAnimateImage(true);
     }, 600); // Delay for the image animation
-    return () => clearTimeout(timer);
+
+    const hideTimer = setTimeout(() => {
+      setShowContent(false);
+    }, 900); // Delay to hide everything after 900ms
+
+    return () => {
+      clearTimeout(imageTimer);
+      clearTimeout(hideTimer);
+    };
   }, []);
 
   return (
-    <Motion animateOnMount={true} variant="fadeDown">
-      <h1 className="text-[40px] font-bold uppercase flex items-center">
-        <span className="">Create</span>
-        <img src={CreateYourSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[46px] transition-all duration-500 ${animateImage ? 'w-[52px] opacity-100' : 'w-0 opacity-0'}`} />
-        <span >Your</span>
-      </h1>
+    <Motion animateOnMount={showContent} variant="fadeDown">
+        <h1 className="text-[40px] font-bold uppercase flex items-center">
+          <span className="">Create</span>
+          <img src={CreateYourSVG} alt="create your" className={`inline-block ml-4 mr-4 h-[46px] transition-all duration-500 ${animateImage ? 'w-[52px] opacity-100' : 'w-0 opacity-0'}`} />
+          <span >Your</span>
+        </h1>
     </Motion> 
   )
 }
