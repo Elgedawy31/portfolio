87960e33156a6b696c28546cd02d9f88ca6ebde6
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 21:17:45 +0200

    Implement background fade-out effect in HeroBackground component using useEffect and state management for enhanced user experience.

--- src/components/sections/Hero/HeroBackground.tsx
+++ src/components/sections/Hero/HeroBackground.tsx
@@ -1,11 +1,19 @@
 
+import { useEffect, useState } from "react";
 import { Motion } from "../../../motion/Motion";
 
 function HeroBackground() {
+  const [showBackground, setShowBackground] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowBackground(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <Motion show={true} variant="fadeUp" className="absolute bottom-0 left-0 right-0">
+    <Motion show={showBackground} variant="fadeUp" className="absolute bottom-0 left-0 right-0">
       <div className="w-full h-[70vh] overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('/src/assets/backgrounds/hero-bg.svg')]"></div>
     </Motion>
   )
