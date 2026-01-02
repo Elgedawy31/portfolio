1ef855601e3372453a8715998e702cae4fb80ae9
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-02 21:36:57 +0200

    Enhance CreateYour component with SVG animation and fade-in effect. Integrate Motion for improved visual presentation and manage SVG visibility using useEffect and state.

--- src/components/sections/Hero/CreateYour.tsx
+++ src/components/sections/Hero/CreateYour.tsx
@@ -1,9 +1,22 @@
 
+import { useEffect, useState } from "react";
+import { Motion } from "../../../motion/Motion";
+import CreateYourSVG from "../../../assets/banner/create-your.svg";
+
 function CreateYour() {
+  const [showSVG, setShowSVG] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSVG(true);
+    }, 600);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-[40px] font-bold uppercase" >Create Your</h1>
-    </div>
+    <Motion animateOnMount={true} variant="fadeDown">
+      <h1 className="text-[40px] font-bold uppercase" >Create <img src={CreateYourSVG} alt="create your" className={`inline-block w-10 h-10 transition-opacity duration-500 ${showSVG ? 'opacity-100' : 'opacity-0'}`} /> Your</h1>
+    </Motion>
   )
 }
 
