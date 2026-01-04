3b85a068e6f464d6a9d4b58ac9164cc24ed19104
Ahme-d711 <elgedawyahmed333@gmail.com>
2026-01-04 08:18:58 +0200

    Enhance Header and Hero components by adding a class to the Motion component in Header for improved styling, and introducing a blurred background element in Hero to enhance visual depth and user experience.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import { DotsGridSVG } from '../ui';
 
 function Header() {
   return (
-    <Motion animateOnMount={true} variant="fadeDown">
+    <Motion animateOnMount={true} variant="fadeDown" className="">
       <header className="fixed top-0 left-0 right-0 z-50 p-4 bg-background/70 flex justify-between items-center  shadow-sm shadow-foreground/10">
         <div className="w-8 h-8 bg-foreground rounded-full"></div>
         
--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -6,6 +6,10 @@ import WithMe from "./WithMy"
 function Hero() {
   return (
     <div className="relative h-screen overflow-hidden pt-44 px-4">
+      <div 
+        className="absolute top-0 w-40 h-40 bg-white/50 left-[25%] z-50"
+        style={{ filter: 'blur(5.375rem)' }}
+      ></div>
       <CreateYour />
       <Feature />
       <WithMe />
